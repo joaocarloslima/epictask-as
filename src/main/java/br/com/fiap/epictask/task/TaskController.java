749d16a15c738b6f96614b6a4f1af1dcb05aed9c
Joao <joaocarloslima@me.com>
2023-10-16 21:57:41 -0300

    🔑 personalização do login e logout

--- src/main/java/br/com/fiap/epictask/task/TaskController.java
+++ src/main/java/br/com/fiap/epictask/task/TaskController.java
@@ -25,6 +25,7 @@ public class TaskController {
     @GetMapping
     public String index(Model model, @AuthenticationPrincipal OAuth2User user){
         model.addAttribute("avatar_url", user.getAttribute("avatar_url"));
+        model.addAttribute("username", user.getAttribute("name"));
         model.addAttribute("tasks", service.findAll());
         return "task/index";
     } 
